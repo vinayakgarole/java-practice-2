dbf8535b69b4baecc599f74e4311277c3f0e488f
vinayakgarole <vinayakgarole10@gmail.com>
2022-09-02 10:27:22 +0530

    Sum Of Digit Number

---
+++ src/SumOfDigit.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SumOfDigit {
+    public static void main(String[] args) {
+        int n,r,sum=0;
+        System.out.println("Enter any Number ");
+        Scanner ref=new Scanner(System.in);
+        n=ref.nextInt();
+        while(n>0) {
+            r=n%10;
+            sum=sum+r;
+            n=n/10;
+        }
+        System.out.println("Sum of Digit "+sum);
+    }
+}
